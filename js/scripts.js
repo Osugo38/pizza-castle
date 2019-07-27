a95963ff9cae4084dac3ce7d6df242c3031085a9
Joseck Osugo <josugo38@gmail.com>
2019-07-27 19:22:38 +0300

    add an alert of how much the toppings cost as per the size of the pizza selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,39 @@
-$(document).ready(function(){
-  $(".clickable").click(function(){
-    $(#form-showing).toggle();
-      $(#form-hidden).toggle();
-  });
+function order(){
+  var size = document.getElementById("size").value;
+  var crust= document.getElementById("crust").value;
+  var toppings= document.getElementById("toppings").value;
+  if(size == 1){
+    alert("Toppings of the pizza you have  selected, cotsts of ksh. 300/= on top of what the pizza costs");
+    return false;
+  }
 
-});
+else if (size === 2) {
+  alert("Toppings of the pizza you have  selected, cotsts of ksh. 200/= on top of what the pizza costs");
+  return false;
+}
+else  {
+  alert("Toppings of the pizza you have  selected, cotsts of ksh. 100/= on top of what the pizza costs");
+  return true;
+}
+}
+
+
+
+
+
+// $(document).ready(function(){
+//   $('submit').submit(function{
+//     alert("name, thank you for getting out to us")
+//   });
+// });
+//
+
+
+
+// function selectGender(){
+//   var gender = document.getElementById("list").value;
+//   if (gender == 0) {
+//     alert("Gender must be selected");
+//     return false;
+//   }
+// }
